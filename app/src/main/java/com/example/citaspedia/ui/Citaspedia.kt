522dc7b8e9ce2cca330323fff17bf6e95988456b
DavidMatias44 <daosmaru3667@gmail.com>
2024-05-24 09:07:56 -0600

    Diseño de formulario terminado

--- app/src/main/java/com/example/citaspedia/ui/Citaspedia.kt
+++ app/src/main/java/com/example/citaspedia/ui/Citaspedia.kt
@@ -1,8 +1,18 @@
 package com.example.citaspedia.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,12 +23,21 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.citaspedia.R
 import com.example.citaspedia.ui.theme.CitasPediaTheme
+import com.example.citaspedia.ui.theme.approve_button
+import com.example.citaspedia.ui.theme.main_text
+import com.example.citaspedia.ui.theme.background_form
+import com.example.citaspedia.ui.theme.denied_button
 
 @Composable
 fun Citaspedia() {
@@ -29,12 +48,18 @@ fun Citaspedia() {
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(it).fillMaxWidth()
+            modifier = Modifier
+                .padding(it)
+                .padding(16.dp)
+                .fillMaxWidth()
+                .background(background_form)
         ) {
             Text(
                 text = stringResource(id = R.string.nombre),
                 textAlign = TextAlign.Start,
-                modifier = Modifier.fillMaxWidth().padding(start = 56.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
             )
             OutlinedTextField(
                 value = "",
@@ -50,7 +75,9 @@ fun Citaspedia() {
             Text(
                 text = "Edad:",
                 textAlign = TextAlign.Start,
-                modifier = Modifier.fillMaxWidth().padding(start = 56.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
             )
             OutlinedTextField(
                 value = "",
@@ -62,9 +89,145 @@ fun Citaspedia() {
                 ),
                 isError = false
             )
+
+            Text(
+                text = "Curp:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+
+            Text(
+                text = "Motivo de ingreso:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+
+            Text(
+                text = "Motivo de lesión:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+
+            Text(
+                text = "Peso:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+
+            Text(
+                text = "Temperatura:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+
+            Text(
+                text = "Talla:",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 56.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = MaterialTheme.colorScheme.surface,
+                    unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                    disabledContainerColor = MaterialTheme.colorScheme.surface,
+                ),
+                isError = false
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Row(
+
+            ) {
+                Button(
+                    onClick = {},
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = denied_button
+                    )
+                ) {
+                    Text("Cancelar")
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                Button(
+                    onClick = {},
+                    shape = RectangleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = approve_button
+                    )
+                ) {
+                    Text("Registrar")
+                }
+            }
+            Spacer(modifier = Modifier.padding(16.dp))
         }
         /*
-            TODO: definir los campos de texto y los botones para el formulario
+            TODO: Los botones para el formulario
+            TODO: Poder insertar texto en los campos y guardarlos en variables.
+            TODO: Darle funcionalidad a los botones para que realicen acciones en la db.
+            TODO: Crear las strings en strings.xml
          */
     }
 }
@@ -74,13 +237,23 @@ fun Citaspedia() {
 fun CitaspediaTopAppBar(
     modifier: Modifier = Modifier
 ) {
-    /*
-        TODO: definir colores para la barra superior.
-        TODO: definir tal vez un logo o algo así.
-     */
     CenterAlignedTopAppBar(
         title = {
-            Text(text = stringResource(id = R.string.app_name))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo_citaspedia),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(64.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    color = main_text,
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
         }
     )
 }
--- app/src/main/java/com/example/citaspedia/ui/theme/Color.kt
+++ app/src/main/java/com/example/citaspedia/ui/theme/Color.kt
@@ -2,6 +2,10 @@ package com.example.citaspedia.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val main_text = Color(0xFF135145)
+val background_form = Color(0xFFD9D9D9)
+val denied_button = Color(0xFFC64848)
+val approve_button = Color(0xFFA1DD71)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
